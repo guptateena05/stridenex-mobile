24729a298a4c5734c2416925a545a9886448a54c
guptateena05 <teena.gupta@erpdata.in>
2026-05-15 11:55:13 +0530

    Deleted some of the unused files

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { PublicTabNavigator } from '@/navigation/PublicTabNavigator';
 import { LoginScreen } from '@/screens/Auth/LoginScreen';
 import { SignupScreen } from '@/screens/Auth/SignupScreen';
 import StudentOnboardingScreen from '@/screens/Auth/StudentOnboardingScreen';
-// import { CollegeOnboardingScreen } from '@/screens/Auth/CollegeOnboardingScreen';
-// import { IndustryOnboardingScreen } from '@/screens/Auth/IndustryOnboardingScreen';
-// import { MentorOnboardingScreen } from '@/screens/Auth/MentorOnboardingScreen';
 import { TermsOfUseScreen } from '@/screens/Auth/TermsOfUseScreen';
 import { PrivacyPolicyScreen } from '@/screens/Auth/PrivacyPolicyScreen';
 import { AuthStackParamList } from './types';
@@ -17,14 +13,10 @@ const Stack = createNativeStackNavigator<AuthStackParamList>();
 const AuthNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      {/* <Stack.Screen name="PublicTabs" component={PublicTabNavigator} /> */}
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="WebOnboarding" component={WebOnboarding} />
       <Stack.Screen name="Signup" component={SignupScreen} />
       <Stack.Screen name="StudentOnboarding" component={StudentOnboardingScreen} />
-      {/* <Stack.Screen name="CollegeOnboarding" component={CollegeOnboardingScreen} />
-      <Stack.Screen name="IndustryOnboarding" component={IndustryOnboardingScreen} />
-      <Stack.Screen name="MentorOnboarding" component={MentorOnboardingScreen} /> */}
       <Stack.Screen name="TermsOfUse" component={TermsOfUseScreen} />
       <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicyScreen} />
     </Stack.Navigator>
